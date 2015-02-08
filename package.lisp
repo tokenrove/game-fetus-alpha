6f29ead230ce070dda18a3e36723f0e6a4979a6b
Julian Squires <julian@cipht.net>
2015-02-07 23:40:13 -0500

    Reorder package exports

--- package.lisp
+++ package.lisp
@@ -19,50 +19,50 @@
 	   #:draw-rectangle
 	   #:draw-filled-rectangle
 	   #:draw-triangle
-	   #:draw-filled-triangle
-	   ;; events
-	   #:+ev-quit+
-	   #:+ev-up+
-	   #:+ev-down+
-	   #:+ev-left+
-	   #:+ev-right+
-	   #:+ev-button-a+
-	   #:+ev-button-b+
-	   #:event-init
-	   #:event-shutdown
-	   #:wipe-events
-	   #:event-update
-	   #:event-pressedp
-	   #:get-key-event
-	   ;; timer
-	   #:timer-get-ticks
-	   #:timer-start-frame
-	   #:timer-end-frame
-	   ;; font
-	   #:font-init
-	   #:paint-string
-	   #:paint-blended-string
-	   #:paint-shaded-string
-	   #:destroy-font
-	   #:load-font
-	   #:with-font
-	   ;; sprite
-	   #:sprite-x
-	   #:sprite-y
-	   #:sprite-priority
-	   #:sprite-blit-offset
-	   #:new-sprite-from-alist
-	   #:draw-sprite
-	   #:set-sprite-animation
-	   #:create-sprite-manager
-	   #:destroy-sprite-manager
-	   #:add-sprite-to-manager
-	   #:remove-sprite-from-manager
-	   #:update-all-sprites
-	   #:with-sprite-manager
-	   ;; layer
-	   ;; generic editor
-	   #:prompt-for-yes-or-no
-	   #:prompt-for-string
-	   #:prompt-for-integer
-	   #:draw-status-message))
+           #:draw-filled-triangle)
+  ;; events
+  (:export #:+ev-quit+
+           #:+ev-up+
+           #:+ev-down+
+           #:+ev-left+
+           #:+ev-right+
+           #:+ev-button-a+
+           #:+ev-button-b+
+           #:event-init
+           #:event-shutdown
+           #:wipe-events
+           #:event-update
+           #:event-pressedp
+           #:get-key-event)
+  ;; timer
+  (:export #:timer-get-ticks
+           #:timer-start-frame
+           #:timer-end-frame)
+  ;; font
+  (:export #:font-init
+           #:paint-string
+           #:paint-blended-string
+           #:paint-shaded-string
+           #:destroy-font
+           #:load-font
+           #:with-font)
+  ;; sprite
+  (:export #:sprite-x
+           #:sprite-y
+           #:sprite-priority
+           #:sprite-blit-offset
+           #:new-sprite-from-alist
+           #:draw-sprite
+           #:set-sprite-animation
+           #:create-sprite-manager
+           #:destroy-sprite-manager
+           #:add-sprite-to-manager
+           #:remove-sprite-from-manager
+           #:update-all-sprites
+           #:with-sprite-manager)
+  ;; layer
+  ;; generic editor
+  (:export #:prompt-for-yes-or-no
+           #:prompt-for-string
+           #:prompt-for-integer
+           #:draw-status-message))
